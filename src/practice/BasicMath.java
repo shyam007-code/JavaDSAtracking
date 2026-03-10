3674c984d2495514e9929dc6d1ac983d53b17b85
shyam <shyam007zzzo@gmail.com>
2026-03-10 20:41:33 +0530

    finished the Basic math practice

--- src/practice/BasicMath.java
+++ src/practice/BasicMath.java
@@ -1,4 +1,8 @@
 package practice;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
 class CountNumbers{
     public void countnums(int n) {
         int count=0;
@@ -32,6 +36,43 @@ class CountNumbers{
         }
 
     }
+    public void Gcd(int n1, int n2){
+        int gcd=1;
+        for(int i=1;i<min(n1,n2);i++){
+            if(n1%i==0 && n2%i==0){
+                gcd=i;
+            }
+
+
+        }
+        System.out.println(gcd);
+
+    }
+    public void AnotehrApproachGcd(int n1, int n2) {
+        //Using the Eculeadian algorithm
+        int a = n1;
+        int b = n2;
+        while (a > 0 && b > 0) {
+            if (a > b) {
+                a = a % b;
+//
+            } else {
+                b = b % a;
+//
+            }
+        }
+        if (a == 0) {
+            System.out.println("gcd " + b);
+
+        } else {
+
+            System.out.println("gcd " + b);
+
+
+        }
+
+
+    }
 
 
 }
@@ -42,6 +83,7 @@ public class BasicMath {
         //count.countnums(123);
        // System.out.println(count.reverseAnumber(123));
 
-        count.palindrome(1210);
+        //count.palindrome(1210);
+        count.AnotehrApproachGcd(21,36);
     }
 }
